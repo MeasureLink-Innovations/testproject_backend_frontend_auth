cb4f07f4ceb3ac5cce71624508483ea54b9993e3
jodo0627 <joergdomey@gmail.com>
2026-02-15 18:37:55 +0100

    fix: inserted webpack-alias in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "standalone",
     reactCompiler: true,
+    // Webpack alias needed for path mappings (@/...)
+    // Turbopack doesn’t automatically honor tsconfig paths, so we
+    // define the same alias here to match `tsconfig.json`.
+    webpack(config) {
+        if (!config.resolve) config.resolve = {} as any;
+        if (!config.resolve.alias) config.resolve.alias = {} as any;
+        config.resolve.alias['@'] = __dirname;
+        return config;
+    },
 };
 
 export default nextConfig;
